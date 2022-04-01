f90732b804d98ce585fe38789bf4ed0fb955f001
Kathleen Moran <kathleenmoran10@gmail.com>
2022-04-01 16:50:21 -0400

    Add solution to problem 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (x < 0 || y < 0 || typeof(x) !== 'number' || typeof(y) !== 'number') {
+        return 'ERROR';
+    }
 
+    let sum = 0;
+    for (let i = Math.min(x, y); i <= Math.max(x, y); i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
