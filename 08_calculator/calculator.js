c5d2ed5df339549a31da7421bb3c0ebb7eed5f6e
Kathleen Moran <kathleenmoran10@gmail.com>
2022-04-11 12:41:07 -0400

    Add solution to problem 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total, curr) => total + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return (arr.length === 0) ? 0 : arr.reduce((total, curr) => total * curr, 1);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x**y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if (x === 0) {
+    return 1;
+  }
+  
+  let result = 1;
+  while (x > 1) {
+    result *= x;
+    x--;
+  }
+  return result;
 };
 
 // Do not edit below this line
