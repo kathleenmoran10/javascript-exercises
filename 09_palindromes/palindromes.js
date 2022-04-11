6327945d080dafe6dc39d3db8a28892048162382
Kathleen Moran <kathleenmoran10@gmail.com>
2022-04-11 13:22:05 -0400

    Add solution to problem 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    const strippedS = s.match(/\w/g).join('');
+    for (let i = 0, j = strippedS.length - 1; i < strippedS.length / 2; i++, j--) {
+        if (!(strippedS[i].toLowerCase() === strippedS[j].toLowerCase())) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
